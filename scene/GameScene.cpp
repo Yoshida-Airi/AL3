1e28a8623c992aba26fc09ff499d10cfa9a7c9aa
k022g0004@g.neec.ac.jp <k022g0004@g.neec.ac.jp>
2023-04-21 15:04:18 +0900

    01_02完了

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,45 +1,43 @@
 #include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
-#include"ImGuiManager.h"
+#include "ImGuiManager.h"
 #include"PrimitiveDrawer.h"
+#include"AxisIndicator.h"
 
 GameScene::GameScene() {}
 
 GameScene::~GameScene()
 {
-	delete sprite_;
+	delete sprite_; 
 	delete model_;
 	delete debugCamera_;
 }
 
 void GameScene::Initialize() {
 
-
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 
-
-	
-	// ファイル名を指定してテクスチャを読み込む
-	textureHandle_ = TextureManager::Load("sample.png");
-	// スプライトの生成
+	//ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("Sample.png");
+	//スプライトの生成
 	sprite_ = Sprite::Create(textureHandle_, {100, 50});
-
 
 	//3Dモデルの生成
 	model_ = Model::Create();
+
 	//ワールドトランスフォームの初期化
 	worldTransform_.Initialize();
 	//ビュープロジェクションの初期化
 	viewProjection_.Initialize();
 
-
 	//サウンドデータの読み込み
 	soundDataHandle_ = audio_->LoadWave("mokugyo.wav");
 	//音声再生
 	audio_->PlayWave(soundDataHandle_);
+	//音声再生
 	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
 
 	//ライン描画が参照するビュープロジェクションを指定する(アドレス渡し)
@@ -47,8 +45,14 @@ void GameScene::Initialize() {
 
 	//デバッグカメラの生成
 	debugCamera_ = new DebugCamera(1280, 720);
-}
 
+	//軸方向の表示を有効にする
+	AxisIndicator::GetInstance()->SetVisible(true);
+	//軸方向表示が参照するビュープロジェクションを指定する（アドレス渡し）
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
+
+
+}
 
 void GameScene::Update()
 {
@@ -60,7 +64,7 @@ void GameScene::Update()
 	//移動した座標をスプライトに反映
 	sprite_->SetPosition(position);
 
-	//スペースキーを押した瞬間音声を停止する
+	//スペースキーを押した瞬間
 	if (input_->TriggerKey(DIK_SPACE))
 	{
 		//音声停止
@@ -69,11 +73,14 @@ void GameScene::Update()
 
 	//デバッグテキストの表示
 	ImGui::Begin("Debug1");
-	//float3ボックス
+	ImGui::Text("Kamata Tarou %d.%d.%d", 2050, 12, 31);
+	ImGui::End();
+
+	ImGui::Begin("Debug");
+	//float3入力ボックス
 	ImGui::InputFloat3("InputFloat3", inputFloat3);
 	//float3スライダー
 	ImGui::SliderFloat3("SliderFloat3", inputFloat3, 0.0f, 1.0f);
-	/*ImGui::Text("Kamata Tarou %d.%d.%d", 2050, 12, 31);*/
 	ImGui::End();
 
 	//デモウィンドウの表示を有効化
@@ -81,7 +88,7 @@ void GameScene::Update()
 
 	//デバッグカメラの更新
 	debugCamera_->Update();
-	
+
 }
 
 void GameScene::Draw() {
@@ -97,7 +104,6 @@ void GameScene::Draw() {
 	/// ここに背景スプライトの描画処理を追加できる
 	/// </summary>
 
-
 	// スプライト描画後処理
 	Sprite::PostDraw();
 	// 深度バッファクリア
@@ -112,7 +118,8 @@ void GameScene::Draw() {
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
 	//3Dモデル描画
-	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
+	/*model_->Draw(worldTransform_, viewProjection_, textureHandle_);*/
+	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
@@ -120,33 +127,20 @@ void GameScene::Draw() {
 	//ラインを描画する
 	PrimitiveDrawer::GetInstance()->DrawLine3d({0, 0, 0}, {0, 10, 0}, {1.0f, 0.0f, 0.0f, 1.0f});
 
-	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
-
 #pragma endregion
 
 #pragma region 前景スプライト描画
 	// 前景スプライト描画前処理
-	
-
 	Sprite::PreDraw(commandList);
-
-
 
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
 
-
-	sprite_->Draw();
-	
-	
+	/*sprite_->Draw();*/
 
 	// スプライト描画後処理
 	Sprite::PostDraw();
 
-
 #pragma endregion
-
-	
-	
 }
