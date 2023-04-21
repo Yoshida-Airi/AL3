69fcb4c16a4b893114d6a73ccc4826f84668f5d5
k022g0004@g.neec.ac.jp <k022g0004@g.neec.ac.jp>
2023-04-21 15:46:09 +0900

    01_01コピー

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -8,7 +8,6 @@
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
-#include "DebugCamera.h"
 
 /// <summary>
 /// ゲームシーン
@@ -23,7 +22,6 @@ public: // メンバ関数
 
 	/// <summary>
 	/// デストラクタ
-	
 	/// </summary>
 	~GameScene();
 
@@ -47,35 +45,9 @@ private: // メンバ変数
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
 
-
-
-
-	//テクスチャハンドル
-	uint32_t textureHandle_ = 0;
-	//スプライト
-	Sprite* sprite_=nullptr;
-
-	//3Dモデル
-	Model* model_ = nullptr;
-
-	//ワールドトランスフォーム
-	WorldTransform worldTransform_;
-	//ビュープロジェクション
-	ViewProjection viewProjection_;
-
-
-	//サウンドデータハンドル
-	uint32_t soundDataHandle_ = 0;
-	//音声再生ハンドル
-	uint32_t voiceHandle_ = 0;
-
-	//ImGuiで値を入力する変数
-	float inputFloat3[3] = {0, 0, 0};
-
-	//デバッグカメラ
-	DebugCamera* debugCamera_ = nullptr;
-
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
 };
+
+
