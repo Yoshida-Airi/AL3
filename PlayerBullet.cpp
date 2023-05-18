cb9232937343da3e92e2dadd51f902ce3567a4d6
k022g0004@g.neec.ac.jp <k022g0004@g.neec.ac.jp>
2023-05-18 23:57:14 +0900

    フォルダー分けできた

--- PlayerBullet.cpp
+++ PlayerBullet.cpp
@@ -1,1 +1,22 @@
 #include <PlayerBullet.h>
+#include<cassert>
+void PlayerBullet::Initialize(Model* model, const Vector3& position)
+{
+	//NULLポインタチェック
+	assert(model);
+
+	model_ = model;
+	//テクスチャ読み込み
+	textureHandle_ = TextureManager::Load("black.png");
+
+	
+	//引数で受け取った初期座標をセット
+	worldTransform_.translation_=
+}
+
+void PlayerBullet::Update()
+{
+
+}
+
+void PlayerBullet::Draw(ViewProjection viewProjection);
